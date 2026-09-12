e11e33eff428e56a972b7914453104119401afda
BasmalaAtef-dev <basmalaatef50@gmail.com>
2026-09-12 21:52:07 +0300

    Enable Swagger in production

--- backend/Program.cs
+++ backend/Program.cs
@@ -104,12 +104,8 @@ var app = builder.Build();
 
 app.UseMiddleware<AcademyAPI.Middleware.GlobalExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 
