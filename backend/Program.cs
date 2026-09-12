03011dcf5ee1a384ebc0fac28fe022254712572a
BasmalaAtef-dev <basmalaatef50@gmail.com>
2026-09-12 20:59:26 +0300

    Add Dockerfile and prepare backend for Render deployment

--- backend/Program.cs
+++ backend/Program.cs
@@ -16,7 +16,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173" ,
+            "https://YOUR-NETLIFY-APP-NAME.netlify.app"
+
+            )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
