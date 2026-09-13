7e0e5773caae6cfa2815085f636b45d51a49680c
BasmalaAtef-dev <basmalaatef50@gmail.com>
2026-09-14 01:02:12 +0300

    Fix Vercel CORS origin

--- backend/Program.cs
+++ backend/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
             "http://localhost:5173",
             "https://gradia-academy-system.vercel.app",
-            "https://gradia-academy-system-iuqa32d70-basmala4.vercel.app/"
+            "https://gradia-academy-system-iuqa32d70-basmala4.vercel.app"
             )
               .AllowAnyHeader()
               .AllowAnyMethod();
