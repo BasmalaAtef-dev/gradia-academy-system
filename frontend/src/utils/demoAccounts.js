bce6ead2f9da701f40a24567a000de1ab3b8d32a
BasmalaAtef-dev <basmalaatef50@gmail.com>
2026-09-14 21:58:53 +0300

    demo accounts handling

--- frontend/src/utils/demoAccounts.js
+++ frontend/src/utils/demoAccounts.js
@@ -1,5 +1,5 @@
 export const DEMO_ACCOUNTS = [
   { role: 'Admin', email: 'admin@academyapi.com', password: 'Admin@123' },
   { role: 'Teacher', email: 'basmalaatef@gmail.com', password: '123456' },
-  { role: 'Student', email: 'mariam.ali@academyapi.com', password: 'Stud@3001!' },
+  { role: 'Student', email: 'ahmed.hassan@academyapi.com', password: 'Stud@3002!' },
 ];
